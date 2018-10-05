fbf0171b371cb0d605ff88c045b32392eafbd252
Honesty1997 <honesty1997>
2018-10-05 13:30:08 +0800

    Revise overrided field name

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
         )
 
         user.set_password(password)
+        user.is_superuser = False
         user.save()
         return user
 
@@ -31,18 +32,22 @@ class UserManager(BaseUserManager):
         )
 
         user.role_field = 4
+        user.is_superuser = True
+        user.is_active = True
+        user.is_staff = True
+
         user.save()
         return user
 
 class User(AbstractBaseUser, Person, Timestamp):
     STUDENT = 1
     PROFESSOR = 2
-    STAFF = 3
+    EXECUTIVE = 3
     ADMIN = 4
     ROLE_CHOICES = (
         (STUDENT, 'Student'),
         (PROFESSOR, 'Professor'),
-        (STAFF, 'Staff'),
+        (EXECUTIVE, 'Executive'),
         (ADMIN, 'Admin')
     )
 
@@ -50,7 +55,9 @@ class User(AbstractBaseUser, Person, Timestamp):
     USERNAME_FIELD = 'email'
 
     objects = UserManager()
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
@@ -71,7 +78,7 @@ class User(AbstractBaseUser, Person, Timestamp):
         return self.role_field == 2
 
     @property
-    def is_staff(self):
+    def is_executive(self):
         return self.role_field == 3
 
     @property
