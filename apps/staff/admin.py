ae109ac51ec1ad696e9df86aba3f24c3d6f9f81a
Doggy1998 <Doggy1998@users.noreply.github.com>
2018-11-05 04:32:47 +0800

    hw1 todo5 register teacher model to the admin

--- apps/staff/admin.py
+++ apps/staff/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .model import Teacher
 
 # TODO(5): Register Teacher model to the admin, so you can 
 # see a Teacher panel when you open django admin.
+
+admin.site.register(Teacher)
