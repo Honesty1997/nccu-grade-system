4852166b4273f1436befe207bd186ce0614e0f87
lbssone <loveseo1124@gmail.com>
2018-11-15 16:43:57 +0800

    add teacher detailview, change 'ID' of info to 'Student Number, Teacher Number'

--- apps/staff/urls.py
+++ apps/staff/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import TeacherList
+from .views import TeacherList, TeacherDetail
 
 app_name = 'staff'
 
 urlpatterns = [
     path('', TeacherList.as_view(), name='list'),
-    # path('<int:pk>', StudentDetail.as_view(), name='detail'),
+    path('<int:pk>', TeacherDetail.as_view(), name='detail'),
     # path('create', StudentCreate.as_view(), name='create'),
     # path('<int:pk>/update', StudentUpdate.as_view(), name='update'),
 ]
