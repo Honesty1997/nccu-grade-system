4673974926686f11e29c1988eabe2adbe19b9309
PhilipL <honesty1997@gmail.com>
2019-01-04 17:50:32 +0800

    Add access limit for different course owner.

--- apps/main/views.py
+++ apps/main/views.py
@@ -7,10 +7,6 @@ from apps.staff.models import Teacher
 # Create your views here.
 @login_required
 def main(request):
-    # TODO:
-    # 如果request.user是學生，找出所有她們註冊的課程，然後assign到course_list。
-    # 如果request.user是老師，找出所有他們教導的課程。
-    # 如果是admin，就列出前10個最新的course就好。
     if request.user.is_student:
         course_list = request.user.student.course_set.all()
         # course_list = Student.objects.get(email=request.user.account).course_set.all()
---
+++ core/utils/mixin.py
@@ -0,0 +1,31 @@
+from django.http import JsonResponse, HttpResponse
+
+class CourseOwnerMixin:
+    def dispatch(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if request.user.is_teacher:
+            course = self.model.objects.get(pk=pk)
+            if not course.teacher == request.user.teacher:
+                if request.META['HTTP_ACCEPT'] == 'application/json':
+                    message = {
+                        'status': 'error',
+                        'message': 'Not authorized.'
+                    }
+                    return JsonResponse(message, status=403)
+                return HttpResponse('Not allowed', status=403)
+        return super().dispatch(request, *args, **kwargs)
+
+class SubjectOwnerMixin:
+    def dispatch(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if request.user.is_teacher:
+            subject = self.model.objects.get(pk=pk)
+            if not subject.course.teacher == request.user.teacher:
+                if request.META['HTTP_ACCEPT'] == 'application/json':
+                    message = {
+                        'status': 'error',
+                        'message': 'Not authorized.'
+                    }
+                    return JsonResponse(message, status=403)
+                return HttpResponse('Not allowed', status=403)
+        return super().dispatch(request, *args, **kwargs)
